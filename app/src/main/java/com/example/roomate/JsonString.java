f771facc358aac7d98e414c9f2aab24ed0c33d81
KookyZar <49061247+KookyZar@users.noreply.github.com>
2022-06-19 12:36:18 +0900

    Create JsonString.java

---
+++ app/src/main/java/com/example/roomate/JsonString.java
@@ -0,0 +1,24 @@
+package com.example.roomate;
+
+public class JsonString {
+
+    private long kakaoid;
+    private String nickname;
+
+    public void setKakaoid(long kakaoid) {
+        this.kakaoid = kakaoid;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+    public long getKakaoid() {
+        return kakaoid;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
