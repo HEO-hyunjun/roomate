6f925d60e1e4ccbf9a45b25fe732c1c253a95549
Carb06 <Carbonium06@gmail.com>
2022-06-21 20:27:41 +0900

    ProfileScreen에 spinner 추가 xml은 추후 변경하겠음

--- app/src/main/java/com/example/roomate/ProfileScreen.java
+++ app/src/main/java/com/example/roomate/ProfileScreen.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,6 +23,17 @@ import androidx.fragment.app.Fragment;
  */
 public class ProfileScreen extends Fragment {
 
+    //드롭다운 스피너 선언 및 스트링 값
+    private Spinner spinner1;
+    private Spinner spinner2;
+    private Spinner spinner3;
+    private Spinner spinner4;
+    private static final String[] grade = new String[]{"1학년","2학년","3학년","4학년"};
+    private static final String[] age = new String[]{"18","19","20","21","22","23","24","25","26","27","28","29","30+"};
+    private static final String[] gender = new String[]{"남자","여자"};
+    private static final String[] domitory_number = new String[]{"3~5동","8동","9동"};
+
+
     ProfileScreen(){
 
     }
@@ -30,6 +45,62 @@ public class ProfileScreen extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile_screen, container, false);
         ImageButton btnInputMyProfile = (ImageButton) v.findViewById(R.id.btn_my_input_profile);
 
+
+        //스피너
+        spinner1= (Spinner)v.findViewById(R.id.sp_grade);
+        spinner2= (Spinner)v.findViewById(R.id.sp_age);
+        spinner3= (Spinner)v.findViewById(R.id.sp_gender);
+        spinner4= (Spinner)v.findViewById(R.id.sp_dom);
+
+        //학년 1~4학년
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,grade);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {@Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+        {
+            Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
+          }@Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}});
+
+        //나이 18~30+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,age);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {@Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+        {
+            Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
+        }@Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}});
+
+        //성별 남자,여자
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,gender);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {@Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+        {
+            Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
+        }@Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}});
+
+        //기숙사 동 3~5동,8동,9동
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,domitory_number);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {@Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+        {
+            Toast.makeText(getActivity(),Integer.toString(position),Toast.LENGTH_SHORT);
+        }@Override
+        public void onNothingSelected(AdapterView<?> adapterView) {}});
+
+        //성격유형 검사로 들어가는 버튼
         btnInputMyProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
