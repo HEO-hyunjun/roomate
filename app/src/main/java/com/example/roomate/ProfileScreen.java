a2f049e4b1e76484cdfceacb96c81ef850bdeff3
HEO-hyunjun <hhj8276@naver.com>
2022-06-21 23:56:00 +0900

    myprofile 화면에서 프로필사진을 누르면 기본프로필 사진중에서 고를 수 있도록 해놓음   profileimageselect_dialog.xml추가 myprofile화면에서 처음 정보 갱신부분 어느정도 작성 + spinner 변수까지 선언해놓음
    허현준

--- app/src/main/java/com/example/roomate/ProfileScreen.java
+++ app/src/main/java/com/example/roomate/ProfileScreen.java
@@ -1,13 +1,17 @@
 package com.example.roomate;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -27,13 +31,23 @@ public class ProfileScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile_screen, container, false);
+        Dialog profileSelect = new Dialog(getActivity());
+        profileSelect.setContentView(R.layout.profileimageselect_dialog);
+
         ImageButton btnInputMyProfile = (ImageButton) v.findViewById(R.id.btn_my_input_profile);
         Button btnConfirm = (Button) v.findViewById(R.id.confirm);
         EditText myIntroduce = (EditText) v.findViewById(R.id.myIntroduce);
         EditText myName = (EditText) v.findViewById(R.id.myName);
+        ImageButton myProfileImage = (ImageButton) v.findViewById(R.id.profile_myprofile);
+        Spinner sp_grade = (Spinner) v.findViewById(R.id.sp_grade);
+        Spinner sp_age = (Spinner) v.findViewById(R.id.sp_age);
+        Spinner sp_dom = (Spinner) v.findViewById(R.id.sp_dom);
+        Spinner sp_gender = (Spinner) v.findViewById(R.id.sp_gender);
+
         try {
             JSONObject myInfo = Data.readMyInfo();
-            myName.setText(myInfo.getString("Nickname").toString());
+            myName.setText(myInfo.getString("Nickname"));
+            myIntroduce.setText(myInfo.getString("Introduce"));
         } catch (IOException | JSONException e) {
             e.printStackTrace();
         }
@@ -43,6 +57,7 @@ public class ProfileScreen extends Fragment {
             public void onClick(View v) {
                 String strMyIntroduce = myIntroduce.getText().toString();
                 String strMyName = myName.getText().toString();
+                int profileId = myProfileImage.getId();
                 //서버에 post
             }
         });
@@ -54,7 +69,33 @@ public class ProfileScreen extends Fragment {
                 getActivity().startActivity(intent);
             }
         });
-
+        myProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                profileSelect.show();
+                profileSelect.findViewById(R.id.profileImage1).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        myProfileImage.setImageResource(R.drawable.a);
+                        profileSelect.dismiss();
+                    }
+                });
+                profileSelect.findViewById(R.id.profileImage2).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        myProfileImage.setImageResource(R.drawable.b);
+                        profileSelect.dismiss();
+                    }
+                });
+                profileSelect.findViewById(R.id.profileImage3).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        myProfileImage.setImageResource(R.drawable.c);
+                        profileSelect.dismiss();
+                    }
+                });
+            }
+        });
         return v;
     }
 
